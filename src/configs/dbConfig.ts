34fd65922904c153b8db9448a537fc632e806b65
yaaarslv <savenkovyaroslav53@gmail.com>
2024-06-19 01:43:12 +0300

    added .env connection data

--- src/configs/dbConfig.ts
+++ src/configs/dbConfig.ts
@@ -1,19 +1,18 @@
 import 'reflect-metadata';
-import {DataSource} from 'typeorm';
-import {Task} from '../entities/Task';
-import dotenv from 'dotenv';
+import { DataSource } from 'typeorm';
+import { Task } from '../entities/Task';
+const dotenv = require('dotenv');
 
-dotenv.config();
+dotenv.config({ path: '../.env' });
 
 export const AppDataSource = new DataSource({
     type: 'postgres',
-    host: process.env.PGHOST,
-    username: process.env.PGUSER,
-    password: process.env.PGPASSWORD,
-    database: process.env.PGDATABASE,
+    ssl: true,
+    host: process.env.DB_HOST,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     synchronize: true,
     logging: false,
-    entities: [Task],
-    migrations: [],
-    subscribers: [],
+    entities: [Task]
 });
